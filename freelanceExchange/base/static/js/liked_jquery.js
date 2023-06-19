770045ea1f26a8df8554a66148d9f71d0bad3f62
Mary Ivanova <shulakova.masha@yandex.ru>
2023-06-19 10:57:33 +0300

    перенесла код jquery (кнопка сердца) в другой файл

---
+++ freelanceExchange/base/static/js/liked_jquery.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+
+   $('.my-form').submit(function (event) {
+
+     event.preventDefault();
+     var form = $(this);
+     var url = form.attr('action');
+     var csrf_token = $('input[name="csrfmiddlewaretoken"]', form).val();
+     var task_id = $('input[name="task_id"]', form).val();
+     var button = $(event.target);
+     var action = button.attr('name') + '=' + button.attr('value');
+     $.ajax({
+      type: 'POST',
+      url: url,
+      headers: { 'X-CSRFToken': csrf_token },
+       data: {'task_id': task_id, 'action': action},
+       success: function (response) { console.log('OK');
+      },
+       error: function (xhr, errmsg, err) { console.log(xhr.status + ': ' + xhr.responseText); } }); }); });
