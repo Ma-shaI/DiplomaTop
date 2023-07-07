97e67ce30e81354400ab35c8706bff73f3ff76fd
Mary Ivanova <shulakova.masha@yandex.ru>
2023-07-07 11:35:09 +0300

    поиск по бюджету в тасках

--- freelanceExchange/tasks/utils.py
+++ freelanceExchange/tasks/utils.py
@@ -9,12 +9,19 @@ def search_tasks(request):
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
 
+    budget = Budget.objects.filter(
+        Q(min_price__iregex=search_query) |
+        Q(max_price__iregex=search_query) |
+        Q(fix_price__iregex=search_query)
+    )
     tasks = Task.objects.distinct().filter(
         Q(is_published=True) &
         (Q(title__iregex=search_query) |
          Q(experiences__iregex=search_query) |
          Q(description__iregex=search_query) |
+         Q(budget__in=budget) |
          Q(skills__title__iregex=search_query))
+
     )
     return tasks, search_query
 
